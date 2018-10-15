73a6b8d4aa4764617ee22542bb5b7ab35a35a449
leonardo <leonardo.giannini@cern.ch>
2018-10-15 06:09:02 +0200

    run

--- test/miniAOD17CRAB_config.py
+++ test/miniAOD17CRAB_config.py
@@ -1,27 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("Pippo")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(25) )
 
 process.load('Configuration/StandardSequences/GeometryDB_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load('Configuration/EventContent/EventContent_cff')
-process.GlobalTag.globaltag = '101X_mcRun2_asymptotic_v2'
+process.GlobalTag.globaltag = '102X_mcRun2_asymptotic_v3'
 process.options = cms.untracked.PSet()
 
 #process.options.numberOfThreads = cms.untracked.uint32(1)
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1#000
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
 
-        "root://xrootd-cms.infn.it//store/mc/RunIIFall17MiniAODv2/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/30000/0055C65C-E558-E811-AB0E-008CFA582BF4.root"
-       
+        #"root://xrootd-cms.infn.it//store/mc/RunIIFall17MiniAODv2/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/30000/0055C65C-E558-E811-AB0E-008CFA582BF4.root",
+	"root://xrootd-cms.infn.it//store/mc/RunIIFall17MiniAODv2/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/70000/28F776F6-AA42-E811-ADBC-008CFAFBFDEC.root"
     )#,
     #skipEvents=cms.untracked.uint32(36590)
 )
@@ -29,7 +29,7 @@ process.source = cms.Source("PoolSource",
 
 process.analyzer1 = cms.EDAnalyzer('Analyzer_MINIAOD_new')
 
-outfile='example_x.root'
+outfile='tree.root'
 
 process.TFileService = cms.Service("TFileService",
    fileName = cms.string(outfile) )
--- test/utilw-crab.py
+++ test/utilw-crab.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'testBTAG-17_TT4'
+config.General.requestName = 'testBTAG2_tthadTrue'
 config.General.workArea = '/scratch/lgiannini/bTAG_retrain/CRAB/'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -11,15 +11,23 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'miniAOD17CRAB_config.py'
 
 
-config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM'
+#config.Data.inputDataset = '/QCD_Pt_50to80_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/QCD_Pt_80to120_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/QCD_Pt_120to170_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/QCD_Pt_170to300_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+#config.Data.inputDataset = '/QCD_Pt-15to7000_TuneCP5_Flat_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+config.Data.inputDataset = '/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
+
+#config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM'
+#config.Data.inputDataset = '/TTToHadronic_mtop171p5_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v2/MINIAODSIM'
 #config.Data.inputDataset = '/QCD_Pt_120to170_TuneCP5_13TeV_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
-config.Data.outLFNDirBase = '/store/user/%s/testBTAG-17' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/%s/BTAG-cmssw102x-allflav' % (getUsernameFromSiteDB())
 config.Data.publication = True
-config.Data.outputDatasetTag = 'CRAB3_tutorial_May2015_MC_analysis'
-config.Data.totalUnits = config.Data.unitsPerJob * 10
+config.Data.outputDatasetTag = 'CRAB3_btag_Rerun'
+config.Data.totalUnits = config.Data.unitsPerJob * 500
 
 
 config.Site.storageSite = "T2_IT_Pisa"
